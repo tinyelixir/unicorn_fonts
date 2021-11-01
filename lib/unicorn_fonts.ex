c4911b92bcdca46176a7764eec5c629b83f6167d
Tim Mecklem <timothy@mecklem.com>
2021-10-31 20:52:55 -0400

    Initial commit of font project breakout from unicorn_encourager

---
+++ lib/unicorn_fonts.ex
@@ -0,0 +1,18 @@
+defmodule UnicornFonts do
+  @moduledoc """
+  Documentation for `UnicornFonts`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UnicornFonts.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/unicorn_fonts_test.exs
@@ -0,0 +1,8 @@
+defmodule UnicornFontsTest do
+  use ExUnit.Case
+  doctest UnicornFonts
+
+  test "greets the world" do
+    assert UnicornFonts.hello() == :world
+  end
+end
